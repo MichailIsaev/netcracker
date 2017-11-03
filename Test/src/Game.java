2083139be66a1e692e6008c6756cb960cc1e62a1
Михаил Исаев <michailisaev@Mihails-MacBook-Pro.local>
2017-11-03 15:54:20 +0400

    Some project

---
+++ Test/src/Game.java
@@ -0,0 +1,57 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "Game")
+@SessionScoped
+public class Game implements Serializable {
+    private static final long serialVersionUID=1234567L;
+    private int min,max;
+    private int current;
+    public static final int MORE=0,LESS=1,EQUAL=2;
+    private final String url="play.xhtml";
+    private final String honest="honest.xhtml";
+    private final String wrong="wrong.xhtml";
+    public Game(){}
+    public String  init(){
+        current=(max+min)/2;
+        return url;
+    }
+
+    public String guess(int param) throws Exception {
+
+        switch(param){
+            case MORE:
+                if(max==min) return wrong;
+                min=current;
+                current=(max+min)/2;
+                return url;
+            case LESS:
+                if(max==min) return wrong;
+                max=current;
+                current=(max+min)/2;
+                return url;
+            case EQUAL:
+                return honest;
+            default:
+                throw new Exception("Wrong operation");
+        }
+    }
+    public int getCurrent(){return current;}
+    public void setCurrent(int current){this.current=current;}
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+}
